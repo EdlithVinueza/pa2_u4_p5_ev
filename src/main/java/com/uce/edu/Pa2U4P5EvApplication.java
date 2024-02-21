ad17a7898e2ecc4533a885404b68da9c96ef2b7e
EdlithVinueza <eavinuezaz1@uce.edu.ec>
2024-02-20 20:26:51 -0500

    taller37

---
+++ src/main/java/com/uce/edu/Pa2U4P5EvApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U4P5EvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U4P5EvApplication.class, args);
+	}
+
+}
